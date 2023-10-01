1b83c1778e3123b56d0282313ea8e5f5bef13a59
Claude BUENO <hello@bueno.fr>
2023-10-01 17:26:56 +0200

    clean-up code

--- src/applicationjavaoc/Capitale.java
+++ src/applicationjavaoc/Capitale.java
@@ -8,23 +8,24 @@ public class Capitale extends Ville {
     private String monument;
 
     //Constructeur par défaut
-    public Capitale(){
+    public Capitale() {
         //Ce mot clé appelle le constructeur de la classe mère
         super();
         monument = "aucun";
     }
 
     //Constructeur d'initialisation de capitale
-    public Capitale(String nom, int hab, String pays, String monument){
+    public Capitale(String nom, int hab, String pays, String monument) {
         super(nom, hab, pays);
         this.monument = monument;
     }
 
     /**
      * Description d'une capitale
+     *
      * @return String retourne la description de l'objet
      */
-    public String decrisToi(){
+    public String decrisToi() {
         String str = super.decrisToi() + "\n \t ==>> " + this.monument + " est un monument de " + nomVille;
 
         return str;
@@ -34,11 +35,13 @@ public class Capitale extends Ville {
      * @return le nom du monument
      */
     public String getMonument() {
+
         return monument;
     }
 
     //Définit le nom du monument
     public void setMonument(String monument) {
+
         this.monument = monument;
     }
 }
--- src/applicationjavaoc/JavaApplicationOC.java
+++ src/applicationjavaoc/JavaApplicationOC.java
@@ -1,31 +1,31 @@
-    package applicationjavaoc;
+package applicationjavaoc;
+
+/**
+ * Created by claude on 05/09/2016.
+ */
+public class JavaApplicationOC {
 
     /**
-     * Created by claude on 05/09/2016.
+     * @param args the command line arguments
      */
-    public class JavaApplicationOC {
+    public static void main(String[] args) {
+        Ville v = new Ville();
+        Ville v1 = new Ville("Marseille", 870321, "France");
+        Ville v2 = new Ville("Lyon", 522228, "France");
 
-        /**
-         * @param args the command line arguments
-         */
-        public static void main(String[] args) {
-            Ville v = new Ville();
-            Ville v1 = new Ville("Marseille", 1236, "France");
-            Ville v2 = new Ville("Rio", 321654, "Brésil");
-
-            System.out.println("\n\n"+v1.decrisToi());
-            System.out.println(v.decrisToi());
-            System.out.println(v2.decrisToi()+"\n\n");
-            System.out.println(v1.comparer(v2));
+        System.out.println("\n\n" + v1.decrisToi());
+        System.out.println(v.decrisToi());
+        System.out.println(v2.decrisToi() + "\n\n");
+        System.out.println(v1.comparer(v2));
 
             /*
               Si vous ne lui mettez aucun paramètre, super() renverra le constructeur par défaut de la classe Ville.
              */
-            System.out.println("------------------------------------------");
-            System.out.println("Sans paramètre super renvoie le constructeur");
-            System.out.println("------------------------------------------ \n");
-            Capitale cap = new Capitale("Paris", 654987, "France", "Notre Dame");
-            System.out.println(cap.decrisToi());
-        }
-
+        System.out.println("------------------------------------------");
+        System.out.println("Sans paramètre super renvoie le constructeur");
+        System.out.println("------------------------------------------ \n");
+        Capitale cap = new Capitale("Paris", 2145906, "France", "Notre Dame");
+        System.out.println(cap.decrisToi());
     }
+
+}
